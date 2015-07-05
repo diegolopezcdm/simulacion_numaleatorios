f1dde2c104961b2de9b2dc5da6968151324cdab8
diego lopez <diegolopezcdm@hotmail.com>
2015-07-04 19:35:15 -0500

    algoritmno de generacion de numeros aletorios agregado

--- NumAleatorios/src/main/java/com/diego/proy/NumAleatorios/App.java
+++ NumAleatorios/src/main/java/com/diego/proy/NumAleatorios/App.java
@@ -1,5 +1,8 @@
 package com.diego.proy.NumAleatorios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	List<Integer> lookUpTable = new ArrayList<Integer>(); 
+    	
+    	int raiz=6;
+        int a=6;
+        int c=6;
+        float m=5;     
+        System.out.println("GENERACION DE NUMEROS ALEATORIOS");
+        System.out.println("METODO CONGRUENCIAL");
+        System.out.println();
+        System.out.println("i\tXi\taXi+c\tmod\tri");
+        System.out.println("-----------------------------------------");
+        for (int i = 0; i < 10; i++) {
+        	float raizOld=raiz;
+        	float mult = (raiz *a)+c;
+        	
+			raiz = (int) (mult%m);
+			float ri =((raiz/m));
+			
+			if(lookUpTable.contains(raiz)){
+				break;
+			} else {
+				lookUpTable.add(raiz);	
+			}
+			
+			System.out.print(i+"\t"+raizOld+"\t"+mult+"\t"+raiz+"\t"+ri+"\n");
+		}
+        System.out.println("Longitud : " + lookUpTable.size());
+        System.out.println();
+        System.out.println("Developer: u201321275 Juan Diego Lopez Flores");
+        System.out.println("Derechos Reservados - CopyRight");
     }
 }
