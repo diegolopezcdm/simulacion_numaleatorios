6f7843d8ce263bf9a99f533cfa01119b28d6cd97
diego lopez <diegolopez@gmail.com>
2015-07-13 23:08:12 -0500

    refactor del programa

--- NumAleatorios/src/main/java/com/diego/proy/NumAleatorios/App.java
+++ NumAleatorios/src/main/java/com/diego/proy/NumAleatorios/App.java
@@ -5,66 +5,114 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * Clase para generar numeros aleatorios Ademas, realiza pruebas de uniformidad
+ * y alatoriedad
  */
 public class App {
 	public static void main(String[] args) {
+		App app = new App();
+		app.run();
+	}
 
-		List<Float> lookUpTable = new ArrayList<Float>();
+	private void run() {
+		List<Float> numerosAleatorios = new ArrayList<Float>();
 		Scanner scanner = new Scanner(System.in);
-		
-		int raiz;
-		int a;
-		int c;
+
+		int raiz, a, c;
 		float m;
-		
+
 		System.out
 				.println("///////////////////////////////////////////////////");
 		System.out.println("GENERACION DE NUMEROS ALEATORIOS");
 		System.out
 				.println("///////////////////////////////////////////////////");
 		System.out.println();
-		
+
 		System.out.print("Ingrese valor para la variable raiz : ");
-	    raiz = scanner.nextInt();
-	    System.out.print("Ingrese valor para la variable elemento multiplicativo : ");
-	    a = scanner.nextInt();
-	    System.out.print("Ingrese valor para la variable elemento aditivo : ");
-	    c = scanner.nextInt();
-	    System.out.print("Ingrese valor para la variable modulo : ");
-	    m = scanner.nextInt();
-		
+		raiz = scanner.nextInt();
+		System.out
+				.print("Ingrese valor para la variable elemento multiplicativo : ");
+		a = scanner.nextInt();
+		System.out.print("Ingrese valor para la variable elemento aditivo : ");
+		c = scanner.nextInt();
+		System.out.print("Ingrese valor para la variable modulo : ");
+		m = scanner.nextInt();
+
+		generarNumerosAleatorios(numerosAleatorios, raiz, a, c, m);
+
+		boolean esUniforme = realizarPruebaDeUniformidad(numerosAleatorios);
+
+		if (!esUniforme) {
+			System.out.println();
+			System.out
+					.println("Numeros aleatorios generados no son uniforme, se reiniciara la generacion de numeros aleatorios.");
+			System.out.println("\n\n Reiniciando programa ...\n");
+			run();
+		}
+
+		boolean esAleatorio = realizarPruebaDeAleatoriedad(numerosAleatorios);
+
+		if (!esAleatorio) {
+			System.out.println();
+			System.out
+					.println("Numeros aleatorios generados no son aleatorios, se reiniciara la generacion de numeros aleatorios.");
+			System.out.println("\n\n Reiniciando programa ...\n");
+			run();
+		}
+
+		generarConclusion();
+	}
+
+	private static void generarConclusion() {
+		System.out.println();
+		System.out
+				.println("===================================================");
+		System.out.println("CONCLUSIÓN");
+		System.out
+				.println("===================================================");
+		System.out
+				.println("Los números del conjunto ri pasaron la pruebas de aleatoriedad y uniformidad satisfactoriamente\nEntonces, podemos decir que tenemos un repositorio de números aleatorios");
+
+		System.out.println();
+		System.out.println();
+		System.out.println("Developer: u201321275 Juan Diego Lopez Flores");
+		System.out.println("Derechos Reservados - CopyRight");
+	}
+
+	private void generarNumerosAleatorios(List<Float> numerosAleatorios,
+			int raiz, int a, int c, float m) {
 		System.out.println();
 		System.out.println("METODO CONGRUENCIAL");
 		System.out.println();
 		System.out.println("i\tXi\taXi+c\tmod\tri");
 		System.out.println("-----------------------------------------");
-		
-		int index =0;
-		
+
+		int index = 0;
+
 		while (true) {
-			
+
 			float raizOld = raiz;
 			float mult = (raiz * a) + c;
 
 			raiz = (int) (mult % m);
 			float ri = ((raiz / m));
 
-			if (lookUpTable.contains(ri)) {
+			if (numerosAleatorios.contains(ri)) {
 				break;
 			} else {
-				lookUpTable.add(ri);
+				numerosAleatorios.add(ri);
 			}
 
 			System.out.print(index + "\t" + raizOld + "\t" + mult + "\t" + raiz
 					+ "\t" + ri + "\n");
 			index++;
-			
-		}
-		
-		System.out.println("Longitud : " + lookUpTable.size());
 
+		}
+
+		System.out.println("Longitud : " + numerosAleatorios.size());
+	}
+
+	private boolean realizarPruebaDeUniformidad(List<Float> numerosAleatorios) {
 		System.out.println();
 		System.out
 				.println("===================================================");
@@ -77,11 +125,11 @@ public class App {
 
 		float sum = 0;
 
-		for (Float value : lookUpTable) {
+		for (Float value : numerosAleatorios) {
 			sum = sum + value;
 		}
-		int n = lookUpTable.size();
-		float r = (sum / (lookUpTable.size()));
+		int n = numerosAleatorios.size();
+		float r = (sum / (numerosAleatorios.size()));
 		r = (float) (Math.round(r * 10000.0) / 10000.0);
 		float limiteSuperior = 0;
 		float limiteInferior = 0;
@@ -93,11 +141,7 @@ public class App {
 		String resultado;
 
 		limiteInferior = (float) (0.5 - (alfa * (1 / (Math.sqrt(12 * n)))));
-		// limiteInferior = (float) (Math.round (limiteInferior * 100000.0) /
-		// 10000.0);
 		limiteSuperior = (float) (0.5 + (alfa * (1 / (Math.sqrt(12 * n)))));
-		// limiteSuperior = (float) (Math.round (limiteSuperior * 100000.0) /
-		// 10000.0);
 
 		if (r > limiteInferior && r < limiteSuperior) {
 			esAleatorio = true;
@@ -121,9 +165,13 @@ public class App {
 
 		if (!esAleatorio) {
 			System.out.println("se aborta operacion ...");
-			return;
+			return false;
 		}
 
+		return true;
+	}
+
+	private boolean realizarPruebaDeAleatoriedad(List<Float> numerosAleatorios) {
 		System.out.println();
 		System.out
 				.println("===================================================");
@@ -134,28 +182,34 @@ public class App {
 				.println("determinemos si el siguiente conjunto de números ri tiene valor esperado de 0,5 con un nivel de aceptación de 95%");
 		System.out.println();
 
+		float alfa = (float) 0.05;
+		alfa = alfa / 2;
+		float z = 1 - alfa;
+		alfa = (float) 1.96;
+
 		int c0 = 0;
+		int n = numerosAleatorios.size();
 		boolean mayorMediaFlag = false;
 		float Uc0 = (2 * (n) - 1) / 3;
 		float o2co = (16 * (n) - 19);
 		o2co = o2co / 90;
 
-		for (int i = 0; i < lookUpTable.size(); i++) {
+		for (int i = 0; i < numerosAleatorios.size(); i++) {
 
 			if (i == 0) {
-				mayorMediaFlag = actualizarFlag(lookUpTable, i);
+				mayorMediaFlag = actualizarFlag(numerosAleatorios, i);
 				imprimirFlag(mayorMediaFlag);
 				c0++;
-			} else if (i == lookUpTable.size()) {
+			} else if (i == numerosAleatorios.size()) {
 				break;
 			} else {
 
-				if (lookUpTable.get(i) > 0.50 && mayorMediaFlag) {
+				if (numerosAleatorios.get(i) > 0.50 && mayorMediaFlag) {
 					System.out.print(1);
-				} else if (lookUpTable.get(i) < 0.50 && !mayorMediaFlag) {
+				} else if (numerosAleatorios.get(i) < 0.50 && !mayorMediaFlag) {
 					System.out.print(0);
 				} else {
-					mayorMediaFlag = actualizarFlag(lookUpTable, i);
+					mayorMediaFlag = actualizarFlag(numerosAleatorios, i);
 					imprimirFlag(mayorMediaFlag);
 					c0++;
 				}
@@ -180,27 +234,15 @@ public class App {
 		if (Z0 < alfa) {
 			System.out
 					.println("Z0 es menor que 1.96. No se puede rechazar, ya que los números del conjunto ri son independientes");
+			return true;
 		} else {
 			System.out
 					.println("Z0 no es menor que 1.96. Se debe rechazar, ya que los números del conjunto ri no son independientes");
-			return;
+			return false;
 		}
-		System.out.println();
-		System.out
-				.println("===================================================");
-		System.out.println("CONCLUSIÓN");
-		System.out
-				.println("===================================================");
-		System.out
-				.println("Los números del conjunto ri pasaron la pruebas de aleatoriedad y uniformidad satisfactoriamente\nEntonces, podemos decir que tenemos un repositorio de números aleatorios");
-
-		System.out.println();
-		System.out.println();
-		System.out.println("Developer: u201321275 Juan Diego Lopez Flores");
-		System.out.println("Derechos Reservados - CopyRight");
 	}
 
-	private static void imprimirFlag(boolean mayorMediaFlag) {
+	private void imprimirFlag(boolean mayorMediaFlag) {
 		if (mayorMediaFlag) {
 			System.out.print(1);
 		} else {
@@ -208,7 +250,7 @@ public class App {
 		}
 	}
 
-	private static boolean actualizarFlag(List<Float> lookUpTable, int i) {
+	private boolean actualizarFlag(List<Float> lookUpTable, int i) {
 		boolean mayorMediaFlag;
 		if (lookUpTable.get(i) > 0.50) {
 			mayorMediaFlag = true;
